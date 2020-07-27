682b330279ebfa93285d1307ebea4763acb847a3
Kyle Edwards <kyle.edwards@kitware.com>
2020-07-27 10:48:01 -0400

    Get setup.py verison from d/changelog

--- setup.py
+++ setup.py
@@ -5,11 +5,25 @@
 # https://gitlab.kitware.com/debian/dh-cmake/blob/master/LICENSE for details.
 
 from setuptools import setup
+import debian.changelog
+import os.path
 
+
+def get_current_version():
+    root_dir = os.path.dirname(os.path.abspath(__file__))
+    changelog_file = os.path.join(root_dir, "debian/changelog")
+    with open(changelog_file) as f:
+        changelog = debian.changelog.Changelog(f)
+    return str(changelog.get_version())
+
+def debian_version_to_python_version(version_str):
+    if version_str[-1] == "~":
+        return version_str[:-1] + ".dev1"
+    return version_str
 
 setup(
     name="dh-cmake",
-    version="0.3",
+    version=debian_version_to_python_version(get_current_version()),
     description="Debhelper program for CMake projects",
     url="https://gitlab.kitware.com/debian/dh-cmake",
     author="Kyle Edwards",
